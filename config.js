b92c7a20ea8acf9179044a8002b4618321da7ad6
lordhabaek25 <demonlordhabaek@gmail.com>
2025-11-15 23:49:17 +0200

    Set default SESSION_ID in config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_16eee3499501424544ae39ae4763e676de').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
